f72f02a5a43fb32177f511990f89f4a7810ea560
melaniedenda98 <mdenda@hawaii.edu>
2022-11-07 01:49:38 -0800

    Made the First View Control of About Me

--- ProjectTwoMelanieDenda/ViewController.swift
+++ ProjectTwoMelanieDenda/ViewController.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Tab Labels
+    @IBOutlet weak var firstTabLabel: UILabel!
+    @IBOutlet weak var mysecondLabel: UILabel!
+    @IBOutlet weak var thirdTabLabel: UILabel!
+    @IBOutlet weak var fourthTabLabel: UILabel!
+    @IBOutlet weak var fiftthTabLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.firstTabLabel.text = "My Name is:"
+        self.mysecondLabel.text = "Melanie"
+        self.thirdTabLabel.text = "My Major is:"
+        self.fourthTabLabel.text = "CM: Design and Media"
+        self.fiftthTabLabel.text = "My Spring 2022 Classes are:"
     }
 
 
